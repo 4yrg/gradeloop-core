5e1f995c601b1195ae94282b863c36af9d0f9e6f
Dasun Wickramasooriya <dasun.wickramasooriya@gmail.com>
2025-11-12 11:38:37 +0530

    feat: Add Pydantic models for the API res + reqs

--- apps/ai-services/app/core/config.py
+++ apps/ai-services/app/core/config.py
@@ -1,15 +1,14 @@
 from pydantic_settings import BaseSettings
 
-
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "AI Services Microservice"
+    PROJECT_NAME: str = "AI Services (CIPAS)"
     API_V1_STR: str = "/api/v1"
-
-    # Placeholder for future model paths
-    # MODEL_PATH_UNIXCODER: str = "./models/unixcoder"
+    
+    # Model Paths
+    MODEL_PATH_UNIXCODER: str
+    MODEL_PATH_CODELIAMA: str
 
     class Config:
         env_file = ".env"
-
 
 settings = Settings()
