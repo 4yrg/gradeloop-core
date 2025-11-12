273b97c49b55bbb67fad01fe26a03b943a8dc787
Dasun Wickramasooriya <dasun.wickramasooriya@gmail.com>
2025-11-12 13:33:00 +0530

    feat: add bruno collection to the project

--- apps/ai-services/app/modules/cipas/services.py
+++ apps/ai-services/app/modules/cipas/services.py
@@ -7,18 +7,18 @@ class CIPASService:
         # e.g., self.clone_model = model_cache["unixcoder"]
         print("CIPAS Service Initialized (Mock)")
 
-        async def process_submission(self, payload: SubmissionInput) -> IntegrityReport:
-            # Mock implementation of submission processing
-            print(f"Processing submission {payload.submission_id} for student {payload.student_id}(Mock)")
+    async def process_submission(self, payload: SubmissionInput) -> IntegrityReport:
+        # Mock implementation of submission processing
+        print(f"Processing submission {payload.submission_id} for student {payload.student_id}(Mock)")
 
-            # Return a fixed mock response the matches the IntegrityReport schema
-            return IntegrityReport(
-                submission_id=payload.submission_id,
-                status="processed",
-                is_ai=False,
-                integrity_score=0.95,
-                detected_clones=["sub_001_similar_function"]
-            )
+        # Return a fixed mock response the matches the IntegrityReport schema
+        return IntegrityReport(
+            submission_id=payload.submission_id,
+            status="processed",
+            is_ai=False,
+            integrity_score=0.95,
+            detected_clones=["sub_001_similar_function"]
+        )
 
 
 # Dependency Helper
