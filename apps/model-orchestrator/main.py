a05d0ecf658bf200b9908ead1323ea6be57becac
Dasun Wickramasooriya <dasun.wickramasooriya@gmail.com>
2025-11-23 16:26:03 +0530

    feat: Add the folder structure for the model orchestrator FastAPI project

--- apps/model-orchestrator/main.py
+++ apps/model-orchestrator/main.py
@@ -1,13 +1,20 @@
+
 from fastapi import FastAPI
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+"""
+Activating and Running the FastAPI Project
+To activate
+-  For Windows: source .venv/Scripts/activate
+To run
+- $ uvicorn main:app --reload
+"""
