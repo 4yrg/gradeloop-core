8da6d5ce7e7c38373524017b8b87c4733bbaf6c9
Dasun Wickramasooriya <dasun.wickramasooriya@gmail.com>
2025-11-25 14:53:38 +0530

    chore: Adjust API routing for code_clone_detection module under /cipas

--- apps/model-orchestrator/app/api/routers.py
+++ apps/model-orchestrator/app/api/routers.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter
-from .template_router import cipas_router
-from ...app.models.cipas.code_clone_detection.api.router import router as ciploc_router
+from .cipas_router import cipas_router # Corrected import
 
 api_router = APIRouter()
 
-api_router.include_router(cipas_router, prefix="/test", tags=["Test"])
-api_router.include_router(ciploc_router, prefix="/ciploc", tags=["Clone Detection"])
+api_router.include_router(cipas_router, prefix="/cipas", tags=["Cipas Module"])
