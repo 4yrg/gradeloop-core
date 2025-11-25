97534e17fe95476c47fff91ce9b827836e64297c
Dasun Wickramasooriya <dasun.wickramasooriya@gmail.com>
2025-11-25 14:11:05 +0530

    test: Add tests for FAISS, inference service, and API router

--- apps/model-orchestrator/app/api/routers.py
+++ apps/model-orchestrator/app/api/routers.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from .template_router import cipas_router
+from ...app.models.cipas.code_clone_detection.api.router import router as ciploc_router
 
 api_router = APIRouter()
 
 api_router.include_router(cipas_router, prefix="/test", tags=["Test"])
+api_router.include_router(ciploc_router, prefix="/ciploc", tags=["Clone Detection"])
