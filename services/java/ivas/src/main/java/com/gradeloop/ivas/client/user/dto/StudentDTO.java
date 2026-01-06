eef11e475755b791b887ff538afa73bb9e2656d8
Sudila-Jayasekara <mpsjayasekara@gmail.com>
2026-01-06 09:44:33 +0530

    feat: Add StudentDTO class for student data representation

---
+++ services/java/ivas/src/main/java/com/gradeloop/ivas/client/user/dto/StudentDTO.java
@@ -0,0 +1,18 @@
+package com.gradeloop.ivas.client.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String rollNumber;
+    private Long userId;
+    private Long instituteId;
+}
