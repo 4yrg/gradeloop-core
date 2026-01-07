d7b0f99fa177cf1f079bfac59b53c33e1b41f06f
H4S1NTH4 <127721393+H4S1NTH4@users.noreply.github.com>
2026-01-07 06:57:30 +0530

    fix:proxy disable

--- web/proxy.ts
+++ web/proxy.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export default function proxy(request: NextRequest) {
+        return NextResponse.next();
     const session = request.cookies.get('session')?.value
     const role = request.cookies.get('user_role')?.value
     const { pathname } = request.nextUrl
