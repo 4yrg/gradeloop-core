cbff56e5a25c36e0fe989782178f4bcbaeb6fd59
h4s1nth4 <jmhdilsha@gmail.com>
2026-01-11 13:12:14 +0530

    improve RabbitMQ configuration setup

--- services/java/keystroke-analytics/src/main/java/com/gradeloop/keystrokeanalytics/config/RabbitMQConfig.java
+++ services/java/keystroke-analytics/src/main/java/com/gradeloop/keystrokeanalytics/config/RabbitMQConfig.java
@@ -1,4 +1,4 @@
-package com.gradeloop.authanalytics.config;
+package com.gradeloop.keystrokeanalytics.config;
 
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
